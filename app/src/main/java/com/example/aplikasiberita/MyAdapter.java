b721c6591eb21fc6b366598b09fe77f80d3281ad
Trional Novanza <70523166+trionalnovanza@users.noreply.github.com>
2020-11-18 11:47:25 +0700

    menambahkan kode import

--- app/src/main/java/com/example/aplikasiberita/MyAdapter.java
+++ app/src/main/java/com/example/aplikasiberita/MyAdapter.java
@@ -6,7 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.content.Intent;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
