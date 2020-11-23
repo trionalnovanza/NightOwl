0756d1a59caf634da7acf8fc616efb1ee39c54bd
Trional Novanza <70523166+trionalnovanza@users.noreply.github.com>
2020-11-23 10:08:08 +0700

    menambahkan file about yang menggunakan API

---
+++ app/src/main/java/com/example/aplikasiberita/API/AboutActivity.java
@@ -0,0 +1,82 @@
+package com.example.aplikasiberita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AboutActivity extends AppCompatActivity {
+    TextView deskripsia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        deskripsia = (TextView) findViewById(R.id.deskripsia);
+        getData();
+    }
+
+    void getData(){
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://www.json-generator.com/api/json/get/celYpSEzma?indent=2";
+
+        JSONObject jsonBody = new JSONObject();
+        final String requestBody = jsonBody.toString();
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+//menaruh data JSON kkedalam variabel JSON Object
+                            JSONObject jsonPost = new JSONObject(response.toString());
+
+//men set data ke dalam tampilan
+                            deskripsia.setText(jsonPost.getString("deskripsi"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error Response",error.toString());
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+}
+
+
+
+
+
+
+
